8f9507f654a361bfe4b35bbc8014d2aa6aa71912
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-09-26 14:36:06 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,70 +3,54 @@
 // `state` is an object that persists across program updates. Store data here.
 import { nodes, root, state } from "membrane";
 
+if (!state.comment_listeners) {
+  state.comment_listeners = new Map();
+}
+if (!state.open_issue_alerts) {
+  state.open_issue_alerts = new Map();
+}
+if (!state.review_requested_alerts) {
+  state.review_requested_alerts = new Map();
+}
+if (!state.review_reminders) {
+  state.review_reminders = new Map();
+}
+
 //set up a listener for an org
 export async function configure_org({ org }) {
-  console.log(org)
+  console.log(org);
   await nodes.github.organizations
-    .one({ name:org })
+    .one({ name: org })
     .commentCreated.$subscribe(root.handle_comment);
   await nodes.github.organizations
-    .one({ name:org })
+    .one({ name: org })
     .reviewRequested.$subscribe(root.handle_reviewrequested);
   await nodes.github.organizations
-    .one({ name:org })
+    .one({ name: org })
     .reviewCreated.$subscribe(root.handle_reviewrequested);
+  root.send_open_issues.$cron("0 0 11 * * *");
+  root.timer_remind_review.$cron("0 0 16 * * *");
+  root.timer_remind_review.$cron("0 0 13 * * *");
 }
 
 //allows a user to listen for comments on their PRs (for all orgs whose comment events are subscribed to)
 export async function user_listen_comments({ github_name, slack_id }) {
-  if (state.comment_listeners) {
-    state.comment_listeners.set(github_name, slack_id);
-  } else {
-    state.comment_listeners = new Map([[github_name, slack_id]]);
-  }
-  console.log(`${github_name},${slack_id} started listening`);
+  state.comment_listeners.set(github_name, slack_id);
 }
 
 export async function user_alert_open_issues({ owner, repo, slack_id }) {
   repo = nodes.github.users.one({ name: owner }).repos.one({ name: repo });
-  console.log("repo", repo);
-  if (!state.open_issue_alerts) {
-    state.open_issue_alerts = new Map();
-  }
   state.open_issue_alerts.get(repo)
     ? state.open_issue_alerts.get(repo).push(slack_id)
     : state.open_issue_alerts.set(repo, [slack_id]);
 }
 
 export async function user_alert_review_requested({ github_name, slack_id }) {
-  if (!state.review_requested_alerts) {
-    state.review_requested_alerts = new Map([[github_name, slack_id]]);
-  } else {
-    state.review_requested_alerts.set(github_name, slack_id);
-  }
-}
-
-export async function user_alert_review_requested_1day({
-  github_name,
-  slack_id,
-}) {
-  if (!state.review_requested_alerts_1day) {
-    state.review_requested_alerts_1day = new Map([[github_name, slack_id]]);
-  } else {
-    state.review_requested_alerts_1day.set(github_name, slack_id);
-  }
+  state.review_requested_alerts.set(github_name, slack_id);
 }
 
 export async function send_open_issues() {
   for (let [repo, value] of state.open_issue_alerts.entries()) {
-    // console.log(
-    //   await repo.issues.search({
-    //     q: "is:issue is:open",
-    //     sort: "created",
-    //     order: "desc",
-    //     per_page: 10, // Adjust this number as needed
-    //   })
-    // );
     const issues = await repo.issues.page.items.$query(
       "{ number title user { login } }"
     );
@@ -83,17 +67,11 @@ export async function send_open_issues() {
     );
   }
 }
-// repos.forEach((repo) => {
-
-// });
 
 export async function handle_comment(_, { event }) {
   const username = await event.comment.$pop().$pop().user.login;
-  console.log(username);
   const listener = state.comment_listeners?.get(username);
-  console.log(listener);
   if (listener) {
-    console.log("listener found");
     await nodes.slack.users.one({ id: listener }).sendMessage({
       text: `New comment for ${username}: ${await event.comment.body}`,
     });
@@ -101,36 +79,68 @@ export async function handle_comment(_, { event }) {
 }
 
 export async function handle_reviewrequested(_, { event }) {
-  console.log("requestedreviewer",await event.requestedReviewer,"pullrequest",await event.pullRequest);
   const reviewer = await event.requestedReviewer.login;
   const pullRequest = await event.pullRequest;
-  const {title,url} = await pullRequest.$query("{ title url }")
+  const { title, url } = await pullRequest.$query("{ title url }");
   const owner = await pullRequest.owner;
-  const {login} = await owner.$query("{ login }");
+  const { login } = await owner.$query("{ login }");
   const slack_id = state.review_requested_alerts.get(reviewer);
 
-  if (slack_id){
+  if (slack_id) {
+    //handle alerting about requested reviews
+    await nodes.slack.users.one({ id: slack_id }).sendMessage({
+      text: `Your review has been requested for the following Pull Request: ${title} by ${login} found at ${url}`,
+    });
 
-      //handle alerting about requested reviews
-      await nodes.slack.users.one({ id: slack_id }).sendMessage({
-        text: `Your review has been requested for the following Pull Request: ${title} by ${login} found at ${url}`,
-      });
+    //handle reminding about requested reviews
+    const requested_reviewers = state.review_reminders.get(pullRequest);
+    if (requested_reviewers) {
+      requested_reviewers.set(reviewer, new Date());
+    } else {
+      state.review_reminders.set(
+        pullRequest,
+        new Map([[reviewer, new Date()]])
+      );
+    }
   }
-  //handle reminding about requested reviews
-
-
-
 }
 
 export async function handle_reviewsubmitted(_, { event }) {
-  //handle alerting about requested reviews
-  const reviewer = await event.pullRequest.requested_reviewer.login;
-  // const pr_url =  await event.pull_request_url;
-  // const request = await event;
-  
-  const slack_id = state.review_requested_alerts.get(reviewer);
+  const pr = await event.pullRequestReview;
+  const reviewer = await pr.user;
 
-  //handle reminding about requested reviews
+  //handle removing review request from reminder map
+  state.review_reminders.get(pr).delete(reviewer);
+  if (state.review_reminders.get(pr).size === 0) {
+    //clean up pr if no requested reviewers are left
+    state.review_reminders.delete(pr);
+  }
+}
+
+export async function timer_remind_review(_, { event }) {
+  const current_time = new Date();
+  for (let [key, valueMap] of state.review_reminders) {
+    console.log(`Key: ${key}`);
+
+    // Loop through the Set
+    for (let [subkey, subvalue] of valueMap) {
+      console.log(`  Value: ${subkey}`);
+      if (
+        Math.abs(current_time.getTime() - subvalue.getTime()) /
+        (24 * 60 * 60 * 1000)
+      ) {
+        const slack_id = state.review_requested_alerts.get(subkey);
+        const pullRequest = await key;
+        const { title, url } = await pullRequest.$query("{ title url }");
+        const owner = await pullRequest.owner;
+        const { login } = await owner.$query("{ login }");
+        console.log("id", slack_id);
+        await nodes.slack.users.one({ id: slack_id }).sendMessage({
+          text: `The following pull request is awaiting your review: ${title} by ${login} found at ${url}`,
+        });
+      }
+    }
+  }
 }
 
 export async function test_func() {
@@ -139,7 +149,3 @@ export async function test_func() {
     .repos.one({ name: "directoryNY" })
     .pull_requests.one({ number: 1 });
 }
-
-// export async function configure_git({ org, api_key }) {
-//   nodes.github.configure({ token: api_key });
-// }
